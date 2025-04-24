619e1a836b6fbda109c40bbcafb5461ae984ca2f
pflaumax <pflaumax@gmail.com>
2025-04-24 18:50:44 +0300

    Add database config production

--- website_project/settings.py
+++ website_project/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 
 # Load environment variables
@@ -68,16 +69,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "website_project.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+# Database configuration
+if os.environ.get("DATABASE_URL"):
+    # Use PostgreSQL in production
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
-}
+else:
+    # Use SQLite locally
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
